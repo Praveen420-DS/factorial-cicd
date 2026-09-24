e667a9077bb4fbd26f00c0499b819cfbaa3efa70
Praveen420-DS <praveen420536@gmail.com>
2026-09-24 09:54:04 +0530

    change some python logic

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
 def factorial(n):
 	if n<0:
-		return ValueError("factorial is not defined for negative number")
+		raise ValueError("factorial is not defined for negative number")
 	r=1
 	for i in range(1,n+1):
 		r=r*i
